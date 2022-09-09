1fcf1e572ac2b7e4bb9f5057b66437b758a81293
Lee Ryan <2solimanleeryan@gmail.com>
2022-09-09 16:08:23 +0800

    Allow pad to augment number of pixel through input

--- js/script.js
+++ js/script.js
@@ -14,3 +14,22 @@ for (let pixel = 1; pixel <= pixelSize; pixel++) {
 }
 
 let pixels = document.querySelectorAll('.grid');
+
+size_selector.addEventListener('change', adjustSize);
+
+function adjustSize(e) {
+    setPixelSize(e.target.value);
+}
+
+function setPixelSize(pixelSize) {
+    pad.innerHTML = '';
+    this.pixelSize = Math.pow(pixelSize, 2);
+    pad.style.gridTemplateColumns = `repeat(${pixelSize}, 1fr)`;
+    pad.style.gridTemplateRows = `repeat(${pixelSize}, 1fr)`;
+
+    for (let pixel = 1; pixel <= this.pixelSize; pixel++) {
+        let grid = document.createElement('div');
+        grid.classList.add('grid');
+        pad.appendChild(grid);        
+    }
+}
